0265adced626d420bd6a1188e19b9d4e14eefb12
ThamiresAntunes <thamires.antunes@academico.ifpb.edu.br>
2025-08-25 11:18:33 -0300

    refactor: apagando rotas desnecessárias

--- backend/src/routes/institution.router.ts
+++ backend/src/routes/institution.router.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { createInstitutionController } from "../controllers/institution/create-institution.controller";
 import { deleteInstitutionController } from "../controllers/institution/delete-institution.controller";
 import { getInstitutionController } from "../controllers/institution/get-institution.controller";
-// import { getIdInstitutionController } from "../controllers/institution/get-id-institution.controller";
 import { updateInstitutionController } from "../controllers/institution/update-institution.controller";
 import { updateLocalizationInstitutionController } from "../controllers/institution/update-localization-institution.controller";
 import { validateCnpj } from "../middlewares/institution/validate-cnpj";
@@ -13,9 +12,8 @@ import { checkInstitutionPermission } from "../middlewares/institution/checkInst
 const router = Router();
 
 router.post("/", authenticateToken, validateCnpj, createInstitutionController);
-router.delete("/:id", deleteInstitutionController);
+router.delete("/:id", authenticateToken, validateId, checkInstitutionPermission, deleteInstitutionController);
 router.get("/", getInstitutionController);
-// router.get("/:id", validateId, getIdInstitutionController);
 router.put("/:id", authenticateToken, validateId, checkInstitutionPermission, updateInstitutionController);
 router.patch("/:id", authenticateToken, validateId, checkInstitutionPermission, updateLocalizationInstitutionController);
 
